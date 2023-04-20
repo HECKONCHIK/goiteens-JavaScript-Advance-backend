157d3f87a115a4b1d9cea74736fd96787a89ec22
Oleh Tsap <olehtsap07@gmail.com>
2023-04-20 19:31:44 +0300

    patch

---
+++ src/js/index.js
@@ -0,0 +1,91 @@
+const BASE_URL = 'http://localhost:3000';
+
+// const newMovie = {
+//     title: "HTML",
+//     director: "Oleh Tsap",
+//     genres: [
+//         "Drama",
+//         "Romance"
+//     ],
+//     rating: 8.8
+// }
+
+// const options = {
+//     'method': 'POST',
+//     headers: {
+//         'Content-type': 'application/json'
+//     },
+//     body: JSON.stringify(newMovie)
+// }
+
+// fetch(`${BASE_URL}/movies`, options)
+//     .then(res => res.json())
+//     .then(console.log)
+
+
+
+
+function addNewMovie(newMovie) {
+    const options = {
+        'method': 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(newMovie)
+    }
+    return fetch(`${BASE_URL}/movies`, options)
+    .then(res => res.json())
+    .then(console.log)
+}
+
+// addNewMovie(
+//     {
+//     title: "JS",
+//     director: "Oleh Tsap",
+//     genres: [
+//         "Comedy",
+//         "Drama",
+//         "Romance"
+//       ],
+//       "rating": 8.8
+//     }
+
+// )
+    
+function updateMovieById(update, movieId) {
+    const options = {
+        'method': 'PATCH',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(update)
+    }
+    return fetch(`${BASE_URL}/movies/${movieId}`, options)
+    .then(res => res.json())
+    .then(console.log)
+}
+
+updateMovieById(
+    {
+        "title": "Desktop",
+      "director": "Oleh Tsap",
+      "genres": [
+        "Action",
+        "Adventure",
+        "Drama"
+      ],
+        "rating": 8.8
+    }, 9
+)
+
+// function getMovies() {
+//     fetch('http://localhost:3000/movies')
+//     .then(response => response.json())
+//     .then(console.log)
+// }
+
+// function getMovieById(movieId) {
+//     fetch(`http://localhost:3000/movies/${movieId}`)
+//     .then(response => response.json())
+//     .then(console.log)
+// }
